90f0a99b42c79eb98426dde2539dc10f5c76b14c
Alex <49249097+MUR4SH@users.noreply.github.com>
2021-04-27 14:45:54 +0300

    Position of hint changed

--- app/src/main/java/com/example/keyboardclicker/game_activity.java
+++ app/src/main/java/com/example/keyboardclicker/game_activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.KeyEvent;
@@ -28,6 +29,8 @@ class Chat extends AppCompatActivity {
     private int num=0;
     private String lang="";
     public Chat(Context ctx){
+        Random r = new Random();
+
         this.nickname = ctx.getResources().getStringArray(R.array.names_array)[r.nextInt(20)];
         this.SetLang(ctx);
         this.SetMessage(ctx,0);
@@ -50,6 +53,9 @@ class Chat extends AppCompatActivity {
     }
     public String GetPhrase(){
         return this.phrase;
+    }
+    public int GetNum(){
+        return this.num;
     }
     public void SetMessage(Context ctx,int num){
         if(this.lang.equals("")){
@@ -106,19 +112,60 @@ class Chat extends AppCompatActivity {
 
         return chat_cloud;
     }
+    public LinearLayout GenerateUserMessage(Context ctx){
+        TextView textView_msg = new TextView(ctx);
+        TextView textView_user = new TextView(ctx);
+        LinearLayout chat_cloud = new LinearLayout(ctx);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(10,10,10,0);
 
-    public int CheckString(Context ctx,View v, int keyCode, KeyEvent event, EditText input){
+        params.gravity = Gravity.END;
+        chat_cloud.setLayoutParams(params);
+
+        chat_cloud.setGravity(Gravity.END);
+        chat_cloud.getGravity();
+
+        chat_cloud.setBackgroundResource(R.drawable.chat_cloud_right);
+        chat_cloud.setOrientation(LinearLayout.VERTICAL);
+
+        textView_msg.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        textView_msg.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
+        textView_msg.setText(this.GetPhrase());
+
+        textView_msg.setGravity(Gravity.END);
+        textView_msg.setTextColor(Color.parseColor("#FF000000"));
+
+        textView_user.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        textView_user.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f);
+        if(ctx.getResources().getText(R.string.settings_username).equals("")){
+            textView_user.setText("Just a user");
+        }else{
+            textView_user.setText(ctx.getResources().getText(R.string.settings_username));
+        }
+
+        textView_user.setTextColor(Color.parseColor("#88000000"));
+        textView_user.setGravity(Gravity.END);
+
+        chat_cloud.addView(textView_msg);
+        chat_cloud.addView(textView_user);
+
+        return chat_cloud;
+    }
+
+    public int CheckString(Context ctx, EditText input, TextView hint){
         String sample = this.phrase;
         String user_input = input.getText().toString();
         if(user_input.length()>sample.length()){
             return 0; //Неверно
         }
+        hint.setText(sample.substring(user_input.length()-1,sample.length()));
         for(int i=0;i<user_input.length()-1;i++){
             if(!(user_input.substring(i,i+1).equals(sample.substring(i,i+1)))){
                 return 0; //Неверно
             }
         }
         if(user_input.length()==sample.length()){
+            this.num++;
             return 2; //Строки совпали
         }else {
             return 1; //Подстроки верны
@@ -154,16 +201,36 @@ public class game_activity extends AppCompatActivity {
         Context ctx = this;
 
         EditText input = (EditText)findViewById(R.id.InputText);
-        input.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                user.CheckString(ctx,v,keyCode,event,input);
-                return true;
-            }
-        });
-
         TextView hint = (TextView)findViewById(R.id.TextHint);
         hint.setText(user.GetPhrase());
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                int check = user.CheckString(ctx,input,hint);
+                if(check == 0){
+                    input.setTextColor(Color.parseColor("#FFFF0000"));
+                }else{
+                    input.setTextColor(Color.parseColor("#FF000000"));
+                    if(check == 2){
+                        chat_window.addView(user.GenerateUserMessage(ctx));
+                        user.SetMessage(ctx,user.GetNum());
+                        user.SetPhrase(ctx,user.GetNum());
+                        chat_window.addView(user.GenerateChatMessage(ctx));
+                        hint.setText(user.GetPhrase());
+                        input.setText(null);
+                    }
+                }
+                return;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+
+
 
         /*TextView = (Button)findViewById(R.id.btn_return);//InputText
         btn_rtrn.setOnClickListener(new View.OnClickListener() {
